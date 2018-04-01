c026ab12005dc70426d3cf47e67fd8f4540050e0
gustinurshobah <32300981+gustinurshobah@users.noreply.github.com>
2018-04-01 21:23:01 +0800

    Membuat Atribut dan Behavior

--- Praktek02/src/praktek02/Segitiga.java
+++ Praktek02/src/praktek02/Segitiga.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package praktek02;
 
-/**
- *
- * @author ASUS
- */
 public class Segitiga {
+    double alas;
+    double tinggi;
     
+    void cetakinfo(){
+        System.out.println("alas : "+alas);
+        System.out.println("tinggi : "+tinggi);
+    }
 }
