30196b90cc875922c18ff67fab2b57d65a6bbeef
gustinurshobah <32300981+gustinurshobah@users.noreply.github.com>
2018-04-02 17:27:20 +0800

    Mengisi Nilai Atribut

--- Praktek02/src/praktek02/SegitigaAksi.java
+++ Praktek02/src/praktek02/SegitigaAksi.java
@@ -4,6 +4,9 @@ package praktek02;
 public class SegitigaAksi {
     public static void main(String[] args) {
         Segitiga s1= new Segitiga();
+        
+        s1.alas = 2;
+        s1.tinggi = 5;
     }
     
 }
